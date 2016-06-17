2fd63970eb717cf21f8e93752b51069f26a36c15
anon <gitdev@web.de>
2016-06-17 10:40:03 +0200

    first working file-output

---
+++ Log-Writer/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ofstream myfile;
+    myfile.open("test.txt");
+    myfile << "Hello World" << endl;
+    myfile.close();
+    return 0;
+}
