15d05c76b7ba6efcdd2a19d4384c098407fb87f0
anon <gitdev@web.de>
2016-06-17 10:53:33 +0200

    two versions of writing, once in loop, once per keyboard

--- Log-Writer/main.cpp
+++ Log-Writer/main.cpp
@@ -2,11 +2,30 @@
 #include <fstream>
 
 using namespace std;
+/*
+int main(){
+    while(1) {
+        ofstream myfile;
+        myfile.open("test.txt", std::ios_base::app);
+        myfile << "Hello World" << endl;
+        myfile.close();
+    }
+    return 0;
+}
+*/
 
 int main(){
-    ofstream myfile;
-    myfile.open("test.txt");
-    myfile << "Hello World" << endl;
-    myfile.close();
+    string s;
+    cout << "Enter 'X' to write to the file" << endl;
+    while(1) {
+        cin >> s;
+        if (toupper(s[0]) == 'X') {
+            ofstream myfile;
+            myfile.open("test.txt", std::ios_base::app);
+            myfile << "tadam tadam tadam" << endl;
+            myfile.close();
+            cout << "Written to file successfully!" << endl;
+        }
+    }
     return 0;
 }
